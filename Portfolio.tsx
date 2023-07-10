d2f597489d34b8d165e99f050a67b9a9d16dbd71
selfishAltruism <officialkyus@gmail.com>
2023-07-11 01:15:50 +0900

    update Timepoint (add Detail)

--- Portfolio.tsx
+++ Portfolio.tsx
@@ -9,14 +9,25 @@ import "./Portfoilo.css";
 export const MAXLENGTH_TIMEBAR = 3000;
 
 function Portfolio() {
+  const { setPageChangeState } = useContext(context);
+
   return (
     <>
+      <div
+        id="portfolioTitle"
+        onClick={() => {
+          setPageChangeState(0);
+        }}
+      >
+        <p>Kang Min Kyu's Development Portfolio</p>
+      </div>
+
       <Timepoint
         barColor="dodgerblue"
         barTop={0}
         barLength={MAXLENGTH_TIMEBAR}
-        startTime="2020.03.01"
-        endTime="to be continued.."
+        startTime="2020.03.01 ~ today"
+        title="중앙대학교 소프트웨어학과 입학"
       />
 
       <div id="gitLink2">
--- Timepoint.tsx
+++ Timepoint.tsx
@@ -4,67 +4,101 @@ import { context } from "./App";
 
 import "./Timepoint.css";
 
+import { IoIosArrowUp } from "react-icons/io";
+
 type Props = {
   barColor: string;
   barTop: number;
   barLength: number;
   startTime: string;
-  endTime: string;
+  title: string;
 };
 
 const timebarStyle = {
   background: "black",
-  top: "40px",
+
+  top: "70px",
+
   height: "3000px",
 };
 
 const startTimeStyle = {
-  top: "20px",
+  top: "64px",
+  left: "110px",
+
   color: "black",
 };
 
-const endTimeStyle = {
-  top: "3020px",
-  color: "black",
+const titleStyle = {
+  top: "38px",
+  left: "0px",
+
+  width: "100vw",
+
+  color: "#323232",
+};
+
+const DetailBlockStyle = {
+  background: "black",
+
+  top: "70px",
 };
 
 const startTimeLineStyle = {
   background: "black",
-  top: "43px",
-};
 
-const endTimeLineStyle = {
-  background: "black",
-  top: "3043px",
+  top: "101px",
 };
 
 function Timepoint(props: Props) {
   timebarStyle.background = `${props.barColor}`;
-  timebarStyle.top = `${props.barTop + 40}px`;
+  timebarStyle.top = `${props.barTop + 70}px`;
   timebarStyle.height = `${props.barLength}px`;
 
   startTimeStyle.color = `${props.barColor}`;
-  endTimeStyle.color = `${props.barColor}`;
-  startTimeStyle.top = `${props.barTop + 40 - 14}px`;
-  endTimeStyle.top = `${props.barTop + props.barLength + 40 - 35}px`;
+  startTimeStyle.top = `${props.barTop + 70 - 6}px`;
+
+  titleStyle.top = `${props.barTop + 70 - 32}px`;
+
+  DetailBlockStyle.background = `${props.barColor}`;
+  DetailBlockStyle.top = `${props.barTop + 70}px`;
 
   startTimeLineStyle.background = `${props.barColor}`;
-  endTimeLineStyle.background = `${props.barColor}`;
-  startTimeLineStyle.top = `${props.barTop + 40 - 14 + 23}px`;
-  endTimeLineStyle.top = `${props.barTop + props.barLength + 40 - 35 + 23}px`;
+  startTimeLineStyle.top = `${props.barTop + 70 + 31}px`;
+
+  const [fcCollapseState, setfcCollapseState] = useState("rotate(0deg)");
+
+  const fcCollapseStyle = {
+    transform: `${fcCollapseState}`,
+  };
+
+  const detailBlockClick = () => {
+    if (fcCollapseState == "rotate(0deg)") {
+      setfcCollapseState("rotate(180deg)");
+      //세부사항이 나오는 파트
+    } else {
+      setfcCollapseState("rotate(0deg)");
+      //세부사항이 들어가는 파트
+    }
+  };
 
   return (
     <>
       <div id="timebar" style={timebarStyle}></div>
 
       <div id="startTimeLine" style={startTimeLineStyle}></div>
+
       <div id="startTime" style={startTimeStyle}>
         {props.startTime}
       </div>
 
-      <div id="endTimeLine" style={endTimeLineStyle}></div>
-      <div id="endTime" style={endTimeStyle}>
-        {props.endTime}
+      <div id="title" style={titleStyle}>
+        <p>{props.title}</p>
+      </div>
+
+      <div id="DetailBlock" style={DetailBlockStyle} onClick={detailBlockClick}>
+        <span>See Detail </span>
+        <IoIosArrowUp size="30" style={fcCollapseStyle} />
       </div>
     </>
   );
